da8174af7711f4ecb9b2cfc2c5e7d69ea540eb9e
Katie Barnett <katie.barnett@bilue.com.au>
2022-03-23 11:44:15 +1100

    Work arounds for DeviceProvider changes

--- okreplay-gradle-plugin/src/main/kotlin/okreplay/DeviceBridge.kt
+++ okreplay-gradle-plugin/src/main/kotlin/okreplay/DeviceBridge.kt
@@ -3,8 +3,11 @@ package okreplay
 import com.android.build.gradle.internal.LoggerWrapper
 import com.android.build.gradle.internal.testing.ConnectedDeviceProvider
 import com.android.builder.testing.api.DeviceException
+import com.android.builder.testing.api.DeviceProvider
 import org.gradle.api.logging.Logger
 import java.io.File
+import java.lang.Exception
+import java.util.concurrent.Callable
 import java.util.concurrent.ExecutionException
 
 internal interface DeviceBridge {
@@ -20,7 +23,7 @@ internal interface DeviceBridge {
 }
 
 internal class RealDeviceBridge(adbPath: File, adbTimeoutMs: Int, private val logger: Logger) : DeviceBridge {
-  private val deviceProvider = ConnectedDeviceProvider(adbPath, adbTimeoutMs, LoggerWrapper(logger))
+  private val deviceProvider: DeviceProvider = ConnectedDeviceProvider(adbPath, adbTimeoutMs, LoggerWrapper(logger), null)
 
   override fun use(block: DeviceBridge.() -> Unit) {
     try {
@@ -36,4 +39,18 @@ internal class RealDeviceBridge(adbPath: File, adbTimeoutMs: Int, private val lo
       deviceProvider.devices.map {
         Device(DeviceInterface.newInstance(it), logger)
       }
+
+  @Throws(DeviceException::class, ExecutionException::class)
+  fun <V> DeviceProvider.use(action: Callable<V>): V {
+    init()
+    return try {
+      try {
+        action.call()
+      } catch (e: Exception) {
+        throw ExecutionException(e)
+      }
+    } finally {
+      terminate()
+    }
+  }
 }
